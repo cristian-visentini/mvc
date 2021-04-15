0f53763cedac506df93efecce8d231b6439d7f71
Cristian Visentini <cristianvisentini@hotmail.com>
2021-04-15 20:04:26 -0300

    problema htaccess resolvido

--- core/Core.php
+++ core/Core.php
@@ -3,7 +3,33 @@ class Core
 {
     public function run()
     {
-        echo "URL: ".$_GET['url'];
+        $url = '/';
+        if(isset($_GET['url'])){
+            $url .= filter_input(INPUT_GET, 'url');
+        }
+        if(!empty($url) && $url != '/'){
+            $url = explode('/', $url);
+            array_shift($url);
+            
+            $CurrentController = $url[0].'Controller';
+            array_shift($url);
+
+            if(isset($url[0]) &&  !empty($url[0])){
+                $CurrentAction = $url[0];
+            }else{
+                $CurrentAction = 'index';
+            }
+
+        }else{
+            $CurrentController = 'homeController';
+            $CurrentAction = 'index';
+
+        }
+
+
+        echo '<hr>';
+        echo 'Controller '.$CurrentController.'<br>';
+        echo 'Action '.$CurrentAction;
         
     }
 }
